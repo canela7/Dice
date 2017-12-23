789639d6984aa300041062350935d4fd040fd93a
canela7 <briancanela@Brians-MBP.socal.rr.com>
2017-12-23 12:18:01 -0800

    Shake motion
    Shake phone to make dices move

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     
@@ -35,17 +35,20 @@ class ViewController: UIViewController {
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         randomDiceIndex1 = Int(arc4random_uniform(6)) //0-5
         randomDiceIndex2 = Int(arc4random_uniform(6))//0-5
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
-        
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
     
     
 }
