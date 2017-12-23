08dc29f7e6f20a15c09af8d7a0bac5abcda29c41
canela7 <briancanela@Brians-MBP.socal.rr.com>
2017-12-22 22:58:32 -0800

    Generate random numbers
    Random number change

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     
     override func viewDidLoad() {
@@ -39,6 +39,9 @@ class ViewController: UIViewController {
         
         randomDiceIndex1 = Int(arc4random_uniform(6)) //0-5
         randomDiceIndex2 = Int(arc4random_uniform(6))//0-5
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
         
     }
